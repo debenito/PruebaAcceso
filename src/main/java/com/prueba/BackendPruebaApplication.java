978dd421299a5c2eff5294e750bf2d1bc9fba056
debenito4 <debenito4>
2022-12-01 12:04:09 +0100

    Doocumentaciones y pruebas

--- src/main/java/com/prueba/BackendPruebaApplication.java
+++ src/main/java/com/prueba/BackendPruebaApplication.java
@@ -2,6 +2,7 @@ package com.prueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -22,6 +23,7 @@ import io.swagger.v3.oas.annotations.info.License;
                         url = "http://www.apache.org/licenses/LICENSE-2.0.html",
                         name = "Apache 2.0"
                 )))
+@EnableCaching
 @SpringBootApplication
 public class BackendPruebaApplication {
 
